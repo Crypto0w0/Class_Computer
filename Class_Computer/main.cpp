37644181c26b2264a5f788a0d4a4101917b707db
Alex Untilov <untiloval80@gmail.com>
2023-03-13 21:42:22 +0100

    Initial Commit

---
+++ Class_Computer/main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+class Processor{
+    int price;
+    string model;
+public:
+    Processor(){
+        price = 0;
+        model = nullptr;
+    }
+    void Print(){
+        cout << "Price: " << price << "\n" << "Model: " << model << endl;
+    }
+};
+
+class DynMemory{
+    int price;
+    string model;
+public:
+    DynMemory(){
+        price = 0;
+        model = nullptr;
+    }
+    void Print(){
+        cout << "Price: " << price << "\n" << "Model: " << model << endl;
+    }
+};
+
+class Videocard{
+    int price;
+    string model;
+public:
+    Videocard(){
+        price = 0;
+        model = nullptr;
+    }
+    void Print(){
+        cout << "Price: " << price << "\n" << "Model: " << model << endl;
+    }
+};
+
+class WebCamera{
+    int price;
+    string model;
+public:
+    WebCamera(){
+        price = 0;
+        model = nullptr;
+    }
+    void Print(){
+        cout << "Price: " << price << "\n" << "Model: " << model << endl;
+    }
+};
+
+class Printer{
+    int price;
+    string model;
+public:
+    Printer(){
+        price = 0;
+        model = nullptr;
+    }
+    void Print(){
+        cout << "Price: " << price << "\n" << "Model: " << model << endl;
+    }
+};
+
+class Mouse{
+    int price;
+    string model;
+public:
+    Mouse(){
+        price = 0;
+        model = nullptr;
+    }
+    void Print(){
+        cout << "Price: " << price << "\n" << "Model: " << model << endl;
+    }
+};
+
+class Computer{
+    Processor p;
+    Videocard v;
+    DynMemory d;
+    WebCamera* w;
+    Printer* pr;
+    Mouse* m;
+public:
+    ~Computer(){
+        delete pr;
+        delete w;
+        delete m;
+    }
+    void Print(){
+        cout << "Processor\n";
+        p.Print();
+        cout << "Videocard\n";
+        v.Print();
+        cout << "Dynamic memory\n";
+        d.Print();
+        cout << "Web Camera\n";
+        w->Print();
+        cout << "Printer\n";
+        pr->Print();
+        cout << "Mouse\n";
+        m->Print();
+        
+    }
+};
+
+int main() {
+    Computer c;
+    c.Print();
+}
